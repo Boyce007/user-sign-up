8df39e6b81b145005ddb0da7f7f3638a7d6ebf92
Boyce007 <diboyce21@students.desu.edu>
2022-07-12 15:41:35 -0400

    Updates

---
+++ src/test/java/Boyce/Adeleke/usersignup/UserSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package Boyce.Adeleke.usersignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
