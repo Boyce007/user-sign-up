b1c4f4307deb7cf4e70122bedb574d3e4b9042b6
Boyce007 <diboyce21@students.desu.edu>
2022-07-12 15:54:53 -0400

    User Model Done

--- src/main/java/Boyce/Adeleke/usersignup/user/model/User.java
+++ src/main/java/Boyce/Adeleke/usersignup/user/model/User.java
@@ -1,5 +1,31 @@
 package Boyce.Adeleke.usersignup.user.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    private String firstName;
+    private String lastName;
+    private String password;
+    @Id
+    private String email;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    public String toString() {
+        return String.format("%s %s %s %s", firstName, lastName, email, password);
+    }
 }
