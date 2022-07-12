bb1547210b09c7686aaea877eaf3479fd1a084fe
Boyce007 <diboyce21@students.desu.edu>
2022-07-12 16:21:48 -0400

    Updates

--- src/main/java/Boyce/Adeleke/usersignup/user/model/User.java
+++ src/main/java/Boyce/Adeleke/usersignup/user/model/User.java
@@ -1,18 +1,18 @@
 package Boyce.Adeleke.usersignup.user.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String firstName;
     private String lastName;
     private String password;
-    @Id
     private String email;
+
 
     public User() {
     }
@@ -24,6 +24,45 @@ public class User {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String toString() {
         return String.format("%s %s %s %s", firstName, lastName, email, password);
