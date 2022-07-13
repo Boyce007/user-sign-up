a7e1459582cf6d4684003a8bd8de1d791ad46fc1
davidupnext <dadeleke31@gmail.com>
2022-07-13 12:28:58 -0400

    updates

--- src/main/java/Boyce/Adeleke/usersignup/user/controllers/UserController.java
+++ src/main/java/Boyce/Adeleke/usersignup/user/controllers/UserController.java
@@ -1,10 +1,13 @@
 package Boyce.Adeleke.usersignup.user.controllers;
 
+import Boyce.Adeleke.usersignup.user.Exceptions.UserCreationException;
 import Boyce.Adeleke.usersignup.user.model.User;
 import Boyce.Adeleke.usersignup.user.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +21,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/Users")
+    @GetMapping("/users")
     public Iterable<User> getAllUsers() {
         return userService.getAllUser();
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) throws UserCreationException {
+        return userService.create(user);
     }
 }
