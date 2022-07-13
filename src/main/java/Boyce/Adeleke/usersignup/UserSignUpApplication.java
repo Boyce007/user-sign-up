633e903d68b71f4fb8deb0a6f6d572c055ecc625
davidupnext <dadeleke31@gmail.com>
2022-07-12 21:59:22 -0400

    running server

--- src/main/java/Boyce/Adeleke/usersignup/UserSignUpApplication.java
+++ src/main/java/Boyce/Adeleke/usersignup/UserSignUpApplication.java
@@ -1,13 +1,29 @@
 package Boyce.Adeleke.usersignup;
 
+import Boyce.Adeleke.usersignup.user.model.User;
+import Boyce.Adeleke.usersignup.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class UserSignUpApplication {
+public class UserSignUpApplication implements CommandLineRunner {
+	private static Logger logger = LoggerFactory.getLogger(UserSignUpApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserSignUpApplication.class, args);
 	}
 
+	@Autowired
+	private UserService UserService;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("David", "Adeleke","pass", "dave@gmail.com");
+		user = UserService.create(user);
+		logger.info(user.toString());
+	}
 }
--- src/main/java/Boyce/Adeleke/usersignup/user/service/UserServiceImpl.java
+++ src/main/java/Boyce/Adeleke/usersignup/user/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package Boyce.Adeleke.usersignup.user.service;
 import Boyce.Adeleke.usersignup.user.model.User;
 import Boyce.Adeleke.usersignup.user.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
     private UserRepo userRepo;
 
